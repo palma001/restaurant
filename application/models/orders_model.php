cacf62b3be8fd3247f5e4df77e0993e52c2a33e8
palma001 <palmaluis1997@gmail.com>
2018-10-14 15:13:32 -0400

    Moodification

--- application/models/orders_model.php
+++ application/models/orders_model.php
@@ -3,54 +3,53 @@
 if (! defined('BASEPATH')) exit ('No direct script access allowed');
 class orders_model extends CI_Model{
 
-	function  __construct()
-	{
-         parent::__construct();
-         $this->load->database();
-         $var="0";
-     }
-
-
-     public function inserte($data)
-     {
-      
-     }
-
-     public  function read($data)
-     {
-           
-     }
-
-      public  function all_orders()
-      {
-         $this->db->join('users','users.user_id=orders.user_id');
-         $query=$this->db->get('orders');  
-     	 if ($query->num_rows()>0) {
-             return $query;
-     	 }else {
-
-              return false;
-     	 }
-      }
-      public function orderss($data)
-      {
-        $this->db->join('products','products.product_id=orders_products.products_id');
-         $this->db->join('users','users.user_id=products.user_id');
-         $this->db->join('orders','orders.order_id=orders_products.orders_id');
-         $this->db->where('orders_id',$data);
-          $query= $this->db->get('orders_products');
-          if ($query->num_rows()>0){ 
-            return $query;
-             } else {
-              return false;
-              }
+  function  __construct()
+  {
+    parent::__construct();
+    $this->load->database();
+    $var="0";
   }
-      public function count()
-      {
-            $query= $this->db->get('orders'); 
-            $numero_filas=$query->num_rows(); 
-             return $numero_filas;
-      }
+
+
+  public function inserte($data)
+  {
+
+  }
+
+  public  function read($data)
+  {
+       
+  }
+
+  public  function all_orders()
+  {
+     $this->db->join('users','users.user_id=orders.user_id');
+     $query=$this->db->get('orders');  
+    if ($query->num_rows()>0) {
+      return $query;
+    }else {
+      return false;
+    }
+  }
+  public function orderss($data)
+  {
+    $this->db->join('products','products.product_id=orders_products.products_id');
+     $this->db->join('users','users.user_id=products.user_id');
+     $this->db->join('orders','orders.order_id=orders_products.orders_id');
+     $this->db->where('orders_id',$data);
+      $query= $this->db->get('orders_products');
+      if ($query->num_rows()>0){ 
+        return $query;
+         } else {
+          return false;
+          }
+  }
+  public function count()
+  {
+        $query= $this->db->get('orders'); 
+        $numero_filas=$query->num_rows(); 
+         return $numero_filas;
+  }
 	
 
 }
