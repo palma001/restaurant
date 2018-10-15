e89dd981ab5ae62c673afe07045698a2dc583c9e
williams <williamst02@hotmail.com>
2018-10-15 18:04:52 -0400

    modificaciones

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -5,17 +5,17 @@ class Home extends CI_Controller {
 	function __construct()
 	{
 		 parent::__construct();
-		$this->load->helper('form');
-		$this->load->helper('url');
-		$this->load->model('orders_model');
-		$this->load->model('customer_model');
-		$this->load->model('products_model');	
+		 $this->load->helper('form');
+		 $this->load->helper('url');
+		 $this->load->model('orders_model');
+		 $this->load->model('customer_model');
+		 $this->load->model('products_model');	
 	}
 
 	public function index()
 	{
 		$data=$this->orders_model->count();
-		$count_customer= $this->customer_model->count_customer();
+		$count_customer= $this->Customer_model->count_customer();
 		$views = $this->products_model->count_views();
 		$this->load->view('layouts/headers');
 		$this->load->view('layouts/navbar');
