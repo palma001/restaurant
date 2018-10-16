5c7a8f0ae3e7ea1d610915f3764cee0147d62cec
williams <williamst02@hotmail.com>
2018-10-16 17:45:56 -0400

    ordenes

--- application/views/orders/orders.php
+++ application/views/orders/orders.php
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-
-        <!-- Begin page -->
-        <div id="wrapper">
-
-         
-            <!-- Left Sidebar End -->
-
-            <!-- ============================================================== -->
-            <!-- Start Page Content here -->
-            <!-- ============================================================== -->
-
-            <div class="content-page">
-                <div class="content">
-                    <!-- Start Content-->
-                    <div class="container-fluid">
-                        
-                        <!-- start page title -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="page-title-box">
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="<?= base_url();?>">Binfrix</a></li>
-                                            <li class="breadcrumb-item"><a>Orders</a></li>
-=======
 <div id="wrapper">
    <div class="content-page">
      <div class="content">
@@ -36,7 +10,6 @@
                                     <ol class="breadcrumb m-0">
                                          <li class="breadcrumb-item"><a href="#">Binfrix</a></li>
                                          <li class="breadcrumb-item"><a href="#">Orders</a></li>
->>>>>>> williams
                                         </ol>
                                 </div>
                                 <h4 class="page-title">Orders</h4>
@@ -44,43 +17,6 @@
                         </div>
                     </div>     
                         <!-- end page title --> 
-<<<<<<< HEAD
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <table id="basic-datatable" class="table table-striped nowrap">
-                                            <thead>
-                                                <tr>
-                                                    <th>Orders Id</th>
-                                                    <th>Full Name</th>
-                                                    <th>Total</th>
-                                                    <th></th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-
-                                            <?php  
-                                            if ($obtener) {
-                                            foreach ($obtener->result() as $obten) { ?>
-                                                <tr>
-                                                <?php $var="1";?>
-
-                                                    <td><?=$obten->order_id;?></td>
-                                                    <td><?=$obten->full_name;?></td>
-                                                    <td><?=$obten->total;?></td>
-                                                    <td>
-                                                       <a  href="<?=base_url('index.php/orders/show/'.$obten->order_id);?>"><i class="fe-eye"></i></a>  
-                                                    </td>
-                                                </tr>
-                                                 <?php  
-
-                                                  }}else{
-
-                                                  }
-
-                                                 ?>
-=======
             <div class="row">
                 <div class="col-12">
                     <div class="card">
@@ -113,7 +49,6 @@
 
                                 }
                                   ?>
->>>>>>> williams
                                                
                               </tbody>
                         </table>
