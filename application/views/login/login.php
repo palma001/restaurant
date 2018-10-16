337da496235495f1f2d7bc883c03d21de757f6e7
williams <williamst02@hotmail.com>
2018-10-16 16:09:33 -0400

    login

---
+++ application/views/login/login.php
@@ -0,0 +1,73 @@
+<div class="account-pages mt-5 mb-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5">
+                        <div class="card">
+
+                            <div class="card-body p-4">
+                                
+                                <div class="text-center w-75 m-auto">
+                                    <a href="index.html">
+                                        <span><img src="assets/images/logo.png" alt="" height="60"></span>
+                                    </a>
+                                    <p class="text-muted mb-4 mt-3">Enter your email address and password to access admin panel.</p>
+                                     <?php 
+                                          $message=$this->session->flashdata('message');
+                                            if ($message) {?>
+                                       <div class="alert alert-danger alert-dismissible bg-danger text-white border-0 fade show"  role="alert">
+                                      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                       </button>
+                                   <?= $message ?>
+                                    </div>
+                                <?php 
+                                    }else {
+                                   }
+                                  ?>
+
+                                </div>
+
+                                <form method="post" action="<?php echo base_url()."index.php/login/login/authenticated";?> ">
+
+                                    <div class="form-group mb-3">
+                                        <label for="emailaddress">Email address</label>
+                                        <input class="form-control" type="email" id="emailaddress" name="email" required="" placeholder="Enter your email">
+                                    </div>
+
+                                    <div class="form-group mb-3">
+                                        <a href="pages-recoverpw.html" class="text-muted float-right"><small></small></a>
+                                        <label for="password">Password</label>
+                                        <input class="form-control" type="password" required="" id="password" name="password" placeholder="Enter your password">
+                                    </div>
+
+                                    <div class="form-group mb-3">
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="checkbox-signin" checked>
+                                            <label class="custom-control-label" for="checkbox-signin">Remember me</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group mb-0 text-center">
+                                        <button class="btn btn-primary btn-block" type="submit"> Log In </button>
+                                    </div>
+
+                                </form>
+                            </div> <!-- end card-body -->
+                        </div>
+                        <!-- end card -->
+
+                        <div class="row mt-3">
+                            <div class="col-12 text-center">
+                                <p class="text-muted"> <a href="pages-register.html" class="text-muted ml-1">Forgot your password?</a></p>
+                                <p class="text-muted">Don't have an account? <a href="pages-register.html" class="text-dark ml-1"><b>Sign Up</b></a></p>
+                            </div> <!-- end col -->
+                        </div>
+                        <!-- end row -->
+
+                    </div> <!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+</div>
+        <!-- end page -->
