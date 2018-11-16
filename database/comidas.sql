933f6af55b6f8b6c8bcdd7e75527086d9c2f6dd3
palma001 <palmaluis1997@gmail.com>
2018-11-16 01:16:37 -0400

    Data base

--- database/comidas.sql
+++ database/comidas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-11-2018 a las 03:33:05
+-- Tiempo de generación: 16-11-2018 a las 06:08:47
 -- Versión del servidor: 10.1.34-MariaDB
 -- Versión de PHP: 7.2.7
 
